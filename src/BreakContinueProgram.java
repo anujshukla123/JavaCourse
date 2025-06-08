ad3d97bef0c6fe6c5fba90a22a9a745a8f6796c6
anujshukla123 <40274446+anujshukla123@users.noreply.github.com>
2025-06-08 18:37:33 +0530

    Break and Continue Program by Anuj Shukla

--- src/BreakContinueProgram.java
+++ src/BreakContinueProgram.java
@@ -4,8 +4,8 @@ public class BreakContinueProgram {
         for (int i = 0; i <= 100; i++) {
             System.out.println(i);
             if (i == 45)
-                continue;//this skips the below code so output will be 45,46 end 47
-            if (i == 45)
+                continue;//Continue:- this skips the below code so output will be 45,46 end 47
+            if (i == 46)
                 break;
 
             System.out.println("end");
